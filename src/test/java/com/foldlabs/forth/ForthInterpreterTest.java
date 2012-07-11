f80558a3a309f359125639a075e32ac7f59fe79e
Arnaud Bailly <Arnaud.Bailly@polyspot.com>
2012-07-11 19:14:49 +0200

    put interpreter expected output in own file

--- src/test/java/com/foldlabs/forth/ForthInterpreterTest.java
+++ src/test/java/com/foldlabs/forth/ForthInterpreterTest.java
@@ -3,6 +3,7 @@ package com.foldlabs.forth;
 import static org.fest.assertions.Assertions.assertThat;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
@@ -18,27 +19,51 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class ForthInterpreterTest {
 
-  private static final File basedir = new File(ForthInterpreter.class.getProtectionDomain().getCodeSource().getLocation().getFile());
-  private final String testCase;
+  private static final File   basedir        = new File(ForthInterpreter.class.getProtectionDomain().getCodeSource().getLocation()
+                                                 .getFile());
+
+  private static List<String> expectedOutput = new ArrayList<>();
+
+  private static FilenameFilter testCaseFiles = new FilenameFilter() {
+    
+    @Override
+    public boolean accept(File dir, String name) {
+      return name.matches("\\d+-.*\\.f");
+    }
+  };
+
+  static {
+    try {
+      expectedOutput = FileUtils.readLines(new File(basedir, "test-cases/expected-output.csv"));
+    } catch (IOException e) {
+      throw new RuntimeException("error while trying to read 'expected-output.csv'", e);
+    }
+  }
+
+  private final String        testCase;
+  private final int           testId;
 
   @Parameters
   public static List<Object[]> testCases() throws IOException {
     List<Object[]> cases = new ArrayList<>();
-    for (File testCase : new File(basedir, "test-cases").listFiles()) {
-      cases.add(new Object[] { FileUtils.readFileToString(testCase) });
+    for (File testCase : new File(basedir, "test-cases").listFiles(testCaseFiles )) {
+      String content = FileUtils.readFileToString(testCase);
+      int testId = Integer.parseInt(testCase.getName().split("-")[0]);
+      cases.add(new Object[] { testId, content });
     }
     return cases;
   }
 
-  public ForthInterpreterTest(String testCase) {
+  public ForthInterpreterTest(int testId, String testCase) {
+    this.testId = testId;
     this.testCase = testCase;
   }
-  
+
   @Test
   public void run_test_case() throws IOException {
     StringWriter writer = new StringWriter();
     new ForthInterpreter().interpret(new StringReader(testCase), writer);
-    assertThat(writer.toString()).matches("7.*4");
+    assertThat(writer.toString()).matches(expectedOutput.get(testId - 1));
   }
 
 }
